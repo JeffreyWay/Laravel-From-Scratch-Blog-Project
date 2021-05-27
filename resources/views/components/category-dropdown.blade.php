601f45398f68e311dc3a48a746e1eb7da2c8b897
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-05-27 14:11:08 -0400

    Episode 42 Complete

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,11 +7,13 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+                     :active="request()->routeIs('home')">All
+    </x-dropdown-item>
 
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             :active='request()->is("categories/{$category->slug}")'
         >{{ ucwords($category->name) }}</x-dropdown-item>
     @endforeach
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -30,7 +30,11 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="/">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+
                 <input type="text"
                        name="search"
                        placeholder="Find something"
