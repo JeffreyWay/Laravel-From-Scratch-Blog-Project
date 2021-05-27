51fb588ab9be2459e8f01564c93febde7ccd183e
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-05-27 14:13:57 -0400

    Episode 43 Complete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,9 +16,10 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, fn($query, $search) =>
-            $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%')
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%')
+            )
         );
 
         $query->when($filters['category'] ?? false, fn($query, $category) =>
