699559df147090f89614d8c597e7be365fed9cf0
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-05-27 14:19:23 -0400

    Episode 44 Complete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,9 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(18)->withQueryString()
         ]);
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,8 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
+
+            {{ $posts->links() }}
         @else
             <p class="text-center">No posts yet. Please check back later.</p>
         @endif
