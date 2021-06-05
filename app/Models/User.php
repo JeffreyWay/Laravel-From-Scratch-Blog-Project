528099c3b3ed22812e2f57155f7bb02205646930
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-06-05 08:47:23 -0400

    Episode 46 Complete

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts()
     {
         return $this->hasMany(Post::class);
